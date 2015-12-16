1f3f8c7b669e849eca3d5ce495e8a29da9d62960
3xx1 <chgm.bunbuker@gmail.com>
2015-12-16 00:13:29 -0800

    Init gulp tasks setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rhizome
 Google Chrome Extension
+
+## How to Setup Git Repo
+1. Install Node.js, this project is dependent on v4.2.3 LTS as of December 12, 2015.
+You can find a link to download the package through this url:
+https://nodejs.org/en/
+
+2. Clone the repository to your local.
+Run `git clone https://github.com/TropicalDandies/rhizome.git` on your terminal.
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+
+// Loading Dependencies
+var autoprefixer = require('gulp-autoprefixer');
+var browser = require('browser-sync');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+// Gulp Tasks - Partials
+gulp.task('server', function() {
+  browser({
+    server: {
+      baseDir: 'src/'
+    }
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src('src/**/*.html')
+      .pipe(browser.reload({stream:true}))
+});
+
+gulp.task('js', function() {
+  gulp.src('src/js/**/*.js')
+      .pipe(plumber())
+      .pipe(uglify())
+      .pipe(gulp.dest('src'))
+      .pipe(browser.reload({stream:true}))
+});
+
+gulp.task('sass', function() {
+  gulp.src('src/sass/**/*.scss')
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('src'))
+      .pipe(browser.reload({stream:true}))
+});
+
+gulp.task('default', ['server'], function() {
+  gulp.watch('src/**/*.html', ['html']);
+  gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('dev', ['sass', 'js', 'default']);
