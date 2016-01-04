fc399cc423971c97751cc0f9fc7c40c793a13c5e
Hidekazu Saegusa <hidekazu.saegusa@frogdesign.com>
2016-01-04 11:30:57 -0800

    img play example also added

--- src/js/sketch/window-middle-left/sketch.js
+++ src/js/sketch/window-middle-left/sketch.js
@@ -1,10 +1,27 @@
+var fingerUp, fingerMiddle, fingerDown;
+
+function preload() {
+  fingerUp = loadImage('assets/finger_up.png');
+  fingerMiddle = loadImage('assets/finger_middle.png');
+  fingerDown = loadImage('assets/finger_down.png');
+}
+
 function setup() {
   createCanvas(300, 240);
   background(0);
   noStroke();
-  frameRate(30);
+  frameRate(8);
 }
 
 function draw() {
-  
+  background(0);
+  if(frameCount%4 == 0) {
+    image(fingerUp, 100, 0, 200, 200);
+  }
+  if(frameCount%4 == 1 || frameCount%4 == 3) {
+    image(fingerMiddle, 100, 0, 200, 200);
+  }
+  if(frameCount%4 == 2) {
+    image(fingerDown, 100, 0, 200 , 200);
+  }
 }
