cca21c31b827aa64bf0e93c767cd6253bb05d2a6
Hidekazu Saegusa <hidekazu.saegusa@frogdesign.com>
2016-01-04 11:14:58 -0800

    add more sketches

--- src/js/sketch/window-bottom-middle/sketch.js
+++ src/js/sketch/window-bottom-middle/sketch.js
@@ -1,1 +1,13 @@
-var a = 7;
+function setup() {
+  createCanvas(300, 240);
+  background(0);
+  noStroke();
+  frameRate(30);
+}
+
+function draw() {
+  background(0, 30);
+  fill(random(0,256));
+  var size = random(20, 50);
+  ellipse(random(0, width), random(0, height), size, size);
+}
